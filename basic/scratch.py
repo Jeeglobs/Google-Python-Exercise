7a7a18020943cb7f17d99a0b9386087df3b32422
Jeeglobs <dp.hudgins@hotmail.com>
2023-02-14 20:43:44 -0500

    trying to figure out [::x] in notes.txt

--- basic/scratch.py
+++ basic/scratch.py
@@ -1,3 +1,38 @@
-# words = ['apple', 'board', 'chess', 'dog', 'zipline']
+words = ['apple', 'board', 'chess', 'dog', 'zipline']
+
+# print(words[::0])
+# --> ValueError: slice step cannot be zero
+
+# ===============================
+
+# print(words[::1])
+# --> ['apple', 'board', 'chess', 'dog', 'zipline'] --- prints list "as-is"? --- Same as print(words)??
 
 # print(words[::-1])
+# --> ['zipline', 'dog', 'chess', 'board', 'apple'] --- prints list in reverse order
+
+# ===============================
+
+# print(words[::2])
+# --> ['apple, 'chess', 'zipline'] --- removes every other string??? --- WTF
+
+# print(words[::-2])
+# --> ['zipline', 'chess', 'apple'] --- reverses it and removes every other string???
+
+# ===============================
+
+# print(words[::3])
+# --> ['apple', 'dog'] --- prints index 0 and index 3??
+
+# print(words[::-3])
+# --> ['zipline', 'board'] --- reverses the order and then prints index 0 and index 3?
+
+# ===============================
+
+# print(words[0::4])
+# --> ['apple', 'zipline']
+
+# print(words[2::4])
+# --> ['chess'] --- WTF why does't it print ['chess', 'zipline']
+
+print(words[::-3])
